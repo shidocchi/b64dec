017af5cf8d00ebf4b5ce09c1ce683b6af4fe2353
YOSHIDA Satoshi <syd@timedia.co.jp>
2025-11-15 01:10:12 +0900

    issue_001 rename test file

---
+++ test_b64.py
@@ -0,0 +1,61 @@
+# coding:utf-8
+
+import unittest
+import binascii
+import b64
+
+class B64Test(unittest.TestCase):
+
+  def setUp(self):
+    pass
+
+  def tearDown(self):
+    pass
+
+  def test_01d(self):
+    """raise exception if not enough 1byte"""
+    with self.assertRaises(binascii.Error):
+      b64.b64dec('A')
+
+  def test_02d(self):
+    """decode 2digit into 1byte"""
+    self.assertEqual('00', b64.b64dec('AA'))
+    self.assertEqual('01', b64.b64dec('AQ'))
+    self.assertEqual('fe', b64.b64dec('_g'))
+    self.assertEqual('ff', b64.b64dec('_w'))
+
+  def test_03d(self):
+    """decode 3digit into 2byte"""
+    self.assertEqual('0000', b64.b64dec('AAA'))
+    self.assertEqual('0001', b64.b64dec('AAE'))
+    self.assertEqual('fffe', b64.b64dec('__4'))
+    self.assertEqual('ffff', b64.b64dec('__8'))
+
+  def test_04d(self):
+    """decode 4digit into 3byte"""
+    self.assertEqual('000000', b64.b64dec('AAAA'))
+    self.assertEqual('000001', b64.b64dec('AAAB'))
+    self.assertEqual('fffffe', b64.b64dec('___-'))
+    self.assertEqual('ffffff', b64.b64dec('____'))
+
+  def test_05d(self):
+    """raise exception if not enough 1byte"""
+    with self.assertRaises(binascii.Error):
+      b64.b64dec('AAAAA')
+
+  def test_06d(self):
+    """decode 6digit into 4byte"""
+    self.assertEqual('00000000', b64.b64dec('AAAAAA'))
+    self.assertEqual('00000001', b64.b64dec('AAAAAQ'))
+    self.assertEqual('fffffffe', b64.b64dec('_____g'))
+    self.assertEqual('ffffffff', b64.b64dec('_____w'))
+
+  def test_11d(self):
+    """decode 11digit into 8byte"""
+    self.assertEqual('0000000000000000', b64.b64dec('AAAAAAAAAAA'))
+    self.assertEqual('0000000000000001', b64.b64dec('AAAAAAAAAAE'))
+    self.assertEqual('fffffffffffffffe', b64.b64dec('__________4'))
+    self.assertEqual('ffffffffffffffff', b64.b64dec('__________8'))
+
+if __name__ == "__main__":
+    unittest.main()
